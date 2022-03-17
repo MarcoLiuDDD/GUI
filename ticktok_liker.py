ce839570682fa0e3205725aca32eed888e0d8f76
MarcoLiuDDD <101748352+MarcoLiuDDD@users.noreply.github.com>
2022-03-17 17:32:53 -0400

    Create ticktok_liker.py

---
+++ ticktok_liker.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+time.sleep(3) # time to set up window
+for i in range (100)
+    pyautogui.moveTo (500, 500) # click photo to like, need to manually figure out
+    pyautogui.doubleClick()
+    pyautogui.moveTo (800, 900) # click next
