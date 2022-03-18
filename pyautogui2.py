3e9852a1057cb238d5bbd13f2e15383b55467ddb
MarcoLiuDDD <101748352+MarcoLiuDDD@users.noreply.github.com>
2022-03-17 20:52:20 -0400

    Update pyautogui2.py

--- pyautogui2.py
+++ pyautogui2.py
@@ -17,3 +17,11 @@ pyautogui.press("enter") # press keyboard
 # screenshot
 pyautogui.screenshot("name.png") # save picture
 
+# look for button location
+button7location = pyautogui.locateOnScreen('calc7key.png')
+# button7location:
+# Box(left=1416, top=562, width=50, height=41)
+button7point = pyautogui.center(button7location)
+button7x, button7y = button7point
+pyautogui.click(button7x, button7y)  # clicks the center of where the 7 button was found
+pyautogui.click('calc7key.png') # a shortcut version to click on the center of where the 7 button was found
